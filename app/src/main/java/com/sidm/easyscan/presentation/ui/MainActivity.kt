3d4ff4a17f16e4b04937961db8baff086b3a9123
Manuel Lopes <manuel_clopes@outlook.com>
2021-10-23 15:48:51 +0100

    Initial commit

---
+++ app/src/main/java/com/sidm/easyscan/presentation/ui/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sidm.easyscan.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sidm.easyscan.R
+import com.sidm.easyscan.presentation.ui.fragments.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val homeFragment by lazy { HomeFragment() }
+    private val aboutFragment by lazy { AboutFragment() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadFragment(homeFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.home -> {
+                    loadFragment(homeFragment)
+                    true
+                }
+
+
+                R.id.about -> {
+                    loadFragment(aboutFragment)
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navigationContainer, fragment)
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/sidm/easyscan/presentation/ui/fragments/AboutFragment.kt
@@ -0,0 +1,20 @@
+package com.sidm.easyscan.presentation.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sidm.easyscan.R
+
+class AboutFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+}
