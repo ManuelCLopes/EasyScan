633952ba007aea7492a98a49045ed5023d1527c2
Manuel Lopes <manuel_clopes@outlook.com>
2021-10-27 16:10:51 +0100

    read & write - Firebase Database get text from image gallery

--- app/src/main/java/com/sidm/easyscan/presentation/data/model/Document.kt
+++
@@ -1,9 +0,0 @@
-package com.sidm.easyscan.presentation.data.model
-
-import java.lang.ref.Reference
-
-data class Document(
-    val id: String,
-    val user: String,
-    val photo: String,
-    val timestamp: String)
