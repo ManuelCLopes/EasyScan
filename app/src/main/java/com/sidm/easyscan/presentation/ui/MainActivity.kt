5356feb4ea50df147555dfbd65f6a4a3c0e385e2
Manuel Lopes <manuel_clopes@outlook.com>
2021-11-14 22:11:55 +0000

    Bug of app crashing when turning the phone orientation - Solved

--- app/src/main/java/com/sidm/easyscan/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/sidm/easyscan/presentation/ui/MainActivity.kt
@@ -6,8 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sidm.easyscan.R
-import com.sidm.easyscan.presentation.ui.fragments.RepositoryFragment
 import com.sidm.easyscan.presentation.ui.fragments.HomeFragment
+import com.sidm.easyscan.presentation.ui.fragments.RepositoryFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,9 +19,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
 
-        loadFragment(homeFragment)
-
+            loadFragment(homeFragment)
+        }
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
